2f67654138475588120a60b28572c74cf5774cd6
wil004 <79720969+wil004@users.noreply.github.com>
2022-06-21 10:17:09 +0200

    Delete ProductAndShoppingCartRepository.java

--- src/main/java/com/novi/webshop/repository/ProductAndShoppingCartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.novi.webshop.repository;
-
-import com.novi.webshop.model.ProductAndShoppingCart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductAndShoppingCartRepository extends JpaRepository<ProductAndShoppingCart, Long> {
-}
