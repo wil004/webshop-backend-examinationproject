7fa52d9b1c357495b10c2bf5b7f947b13e1a4fa8
wil004 <79720969+wil004@users.noreply.github.com>
2022-07-06 18:53:35 +0200

    Delete ReturnCartRepository.java

--- src/main/java/com/novi/webshop/repository/ReturnCartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.novi.webshop.repository;
-
-import com.novi.webshop.model.ReturnCart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReturnCartRepository extends JpaRepository<ReturnCart, Long> {
-}
